cd3693ad611433db894ef73a18ffcd740edc2604
thomasburleson <thomasburleson@gmail.com>
2024-03-04 19:46:30 -0500

    Lab 1: Create RSM state

---
+++ libs/contacts/data-access/src/store/contacts.state.tsx
@@ -0,0 +1,33 @@
+import { Contact } from '../api/contacts.model';
+import { StoreState } from '@mindspace/di';
+
+// *******************************************************************
+// Types and initializers
+// *******************************************************************
+
+/**
+ * This state is serializable
+ */
+export interface ContactsState extends StoreState {
+  people: Contact[];
+  criteria: string;
+  selectedId: string;
+}
+
+/**
+ * Read-only values computed from existing/updated state
+ */
+export interface ContactsComputedState {
+  selected: Contact | undefined;
+}
+
+/**
+ * This is a simple API meant for use within the
+ * UI layer html templates
+ */
+export interface ContactsAPI {
+  searchBy: (criteria: string) => Promise<boolean>;
+  selectById: (id: string) => Promise<boolean>;
+}
+
+export type ContactsViewModel = ContactsState & ContactsAPI & ContactsComputedState;
